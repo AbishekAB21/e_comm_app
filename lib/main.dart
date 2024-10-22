afab525e90d8c43076f5007f7353bf813710d7cd
AbishekAB21 <abishekab1805@outlook.com>
2024-10-23 02:38:45 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:e_comm_app/provider/bottom_nav_provider.dart';
 import 'package:e_comm_app/provider/cart_provider.dart';
 import 'package:e_comm_app/provider/home_screen_provider.dart';
 import 'package:e_comm_app/screens/splash_screen.dart';
-import 'package:e_comm_app/widget/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
